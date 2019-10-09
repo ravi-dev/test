a9213b4aa14db4b842f529ed180a91015d381031
bkona-alopa <bkona@alopasoft.com>
2019-10-09 22:09:16 +0530

    modified the test script

--- admin/admin.groovy
+++ admin/admin.groovy
@@ -1,12 +1,11 @@
 Binding binding = new Binding();
-freeStyleJob('binding.getVariable​(jobfolder))/kibo-admin') {
+freeStyleJob('DSL-JOBS/kibo-admin-dsl-test') {
     description('testing job dsl on kibo-admin')  
-    
+    label('mac-slave')
     logRotator {
-	    numToKeep(3)
+	    artifactNumToKeep(3)
 	    daysToKeep(3)
 	    }
-
     scm {
         git {
             remote {
@@ -16,51 +15,33 @@ freeStyleJob('binding.getVariable​(jobfolder))/kibo-admin') {
             branch('master')
         }
     }
-    
+    triggers {
+        githubPush()
+    }
     wrappers {
         preBuildCleanup()
     }
-    
-    credentialsBinding {
-        usernamePassword {
-        // Name of an environment variable to be set to the username during the build.
-        usernameVariable('GIT_USRNAME')
-        // Name of an environment variable to be set to the password during the build.
-        passwordVariable('GIT_PASWD')
-        // Credentials of an appropriate type to be set to the variable.
-        credentialsId('bkona-alopa')
-        }
-    }
-
     steps {
-        shell('docker build --build-arg gitusername=${GIT_USRNAME}  --build-arg gitpassword=${GIT_PASWD} -t kibo-admin:latest .')
+        shell('docker build -t kibo-admin:latest .')
         shell('docker save --output kibo-admin.tar kibo-admin:latest')
     }
     publishers {
-        archiveArtifacts("kibo-admin.tar")
+        archiveArtifacts("*.tar")
         extendedEmail {
             recipientList('bks@amalgarx.com')
-            project_replyto('bks@amalgamrx.com')
             defaultSubject("\${DEFAULT_SUBJECT}")
             defaultContent("\${DEFAULT_CONTENT}")
             contentType('text/html')
-            attachBuildLog(false)
             triggers {
                 always {
                     sendTo {
                         recipientList()
-                        project_replyto()
                     }
                 }
             }
         }
     }
     properties{
-        copyArtifactPermission {
-            // Comma-separated list of projects that can copy artifacts of this project.
-            projectNames('kibo-deployer')
-        }
-
 		promotions{
 			promotion {
                 name('Deploy_to_DEV')
@@ -88,4 +69,3 @@ freeStyleJob('binding.getVariable​(jobfolder))/kibo-admin') {
 	    }
     }
 }
-
