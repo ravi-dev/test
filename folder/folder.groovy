2d4e320a3efd90f9c2759e33a382097f90889b9c
bkona-alopa <46893493+bkona-alopa@users.noreply.github.com>
2019-10-10 11:27:53 +0530

    folder modificationb

--- folder/folder.groovy
+++ folder/folder.groovy
@@ -1,7 +1,6 @@
-abstract class MainScript extends Script {
-    def Folder_name = 'test'
-    folder('${Folder_name}') {
-    displayName('${Folder_name}')
-    description('test project')
-}
+Binding binding = new Binding();
+    binding.setVariable("jobfolder", "DSL-JOBS");
+// jobfolder="DSL-JOBS"
+folder('jobfolder') {
+    displayName('jobfolder')
 }
